9e887577d574ce1bb53cb665a84af2739ae16666
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-10-28 18:43:22 +0600

    hide firebace code

--- src/FireBace/FireBace.config.js
+++ src/FireBace/FireBace.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAh2PBJy3Y9iWjXOPGmwvGhAQzyhi3a9v4",
-  authDomain: "skillbd-c7e83.firebaseapp.com",
-  projectId: "skillbd-c7e83",
-  storageBucket: "skillbd-c7e83.appspot.com",
-  messagingSenderId: "678799537019",
-  appId: "1:678799537019:web:32326e79f67e59578443de"
+  apiKey: process.env.REACT_APP_apiKey ,
+  authDomain: process.env.REACT_APP_authDomain ,
+  projectId: process.env.REACT_APP_projectId ,
+  storageBucket: process.env.REACT_APP_storageBucket ,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
+  appId: process.env.REACT_APP_appId 
 };
 
 // Initialize Firebase
